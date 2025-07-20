0597b02f22c56cc5604b1e1e4623f1a3c8e4958e
fyam <fung19971115@gmail.com>
2025-07-21 02:03:30 +0800

    Add username generation and copy functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script lang="ts">
+import {fakerEN} from '@faker-js/faker'
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+            username: '',
+        }
+    },
+    methods: {
+        generateRandomUserName() {
+            return `${fakerEN.word.adjective()}-${fakerEN.word.noun()}-${fakerEN.number.int({max: 9999})}`
+        },
+        handleGenerate() {
+            this.username = this.generateRandomUserName()
+        },
+        handleCopy() {
+            navigator.clipboard.writeText(this.username)
+        },
+    },
+})
 </script>
 
 <template>
+    <div style="display: flex; align-items: center; gap: 8px; margin-bottom: 16px;">
+        <h3 style="margin: 0; min-width: 200px;">{{ username }}</h3>
+        <button @click="handleCopy" :disabled="!username">Copy</button>
+    </div>
+    <button @click="handleGenerate">Generate Username</button>
 </template>
