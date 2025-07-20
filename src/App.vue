d996b5a37fd2f58f7b90a38f525fc0d1e5ba9b57
fyam <fung19971115@gmail.com>
2025-07-21 02:34:32 +0800

    Add username generation options

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,39 @@
 import { fakerEN } from "@faker-js/faker"
 import { defineComponent } from "vue"
 
+export enum CaseType {
+    Lower = "lower",
+    Upper = "upper",
+    Title = "title",
+}
+
 export default defineComponent({
     data() {
         return {
             username: "",
+            caseType: CaseType.Lower,
+            separator: "-",
         }
     },
     methods: {
         generateRandomUserName() {
-            return `${fakerEN.word.adjective()}-${fakerEN.word.noun()}-${fakerEN.number.int({ max: 9999 })}`
+            const adj = fakerEN.word.adjective()
+            const noun = fakerEN.word.noun()
+            const num = fakerEN.number.int({ max: 9999 })
+            let sep = this.separator
+            let adjPart = adj
+            let nounPart = noun
+            if (this.caseType === CaseType.Upper) {
+                adjPart = adj.toUpperCase()
+                nounPart = noun.toUpperCase()
+            } else if (this.caseType === CaseType.Title) {
+                adjPart = adj.charAt(0).toUpperCase() + adj.slice(1)
+                nounPart = noun.charAt(0).toUpperCase() + noun.slice(1)
+            } else {
+                adjPart = adj.toLowerCase()
+                nounPart = noun.toLowerCase()
+            }
+            return `${adjPart}${sep}${nounPart}${sep}${num}`
         },
         handleGenerate() {
             this.username = this.generateRandomUserName()
@@ -29,6 +53,32 @@ export default defineComponent({
             <button @click="handleCopy">Copy</button>
         </div>
         <button @click="handleGenerate">Generate Username</button>
+        <div class="config-row">
+            Case Type:
+            <div class="case-radio-group">
+                <label>
+                    <input type="radio" value="lower" v-model="caseType" />
+                    Lower
+                </label>
+                <label>
+                    <input type="radio" value="upper" v-model="caseType" />
+                    Upper
+                </label>
+                <label>
+                    <input type="radio" value="title" v-model="caseType" />
+                    Title
+                </label>
+            </div>
+        </div>
+        <label class="config-row">
+            Separator:
+            <input
+                type="text"
+                v-model="separator"
+                class="separator-input"
+                maxlength="2"
+            />
+        </label>
     </div>
 </template>
 
@@ -38,7 +88,8 @@ export default defineComponent({
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    min-height: 100vh;
+    min-height: 100svh;
+    gap: 8px;
 }
 
 .username-row {
@@ -51,5 +102,20 @@ export default defineComponent({
 .username {
     margin: 0;
     min-width: 200px;
+}
+
+.separator-input {
+    width: 40px;
+}
+
+.case-radio-group {
+    display: flex;
+    gap: 8px;
+}
+
+.config-row {
+    gap: 16px;
+    display: flex;
+    align-items: center;
 }
 </style>
